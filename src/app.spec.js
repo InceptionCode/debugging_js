01eb4579991c7104487be66a47e061df26d748b3
Darrell Washington <darrellwjr.com@gmail.com>
2018-05-12 04:12:32 -0400

    Added precommit testing and spec.html & fixed broken test

--- src/app.spec.js
+++ src/app.spec.js
@@ -9,14 +9,14 @@ const {JSDOM} = jsdom;
 const MockInitialDB = sinon.mock(initialDB);
 /* global describe it */
 
-describe('Demo App', ()=> {
-  describe('Add A Person', ()=> {
-    it('should add a list element (person) and add person to DB', (done)=>{
-      const demo = fs.readFileSync('./src/demo.html', 'utf-8');
+describe('Demo App', () => {
+  describe('Add A Person', () => {
+    it('should add a list element (person) and add person to DB', (done) => {
+      const demo = fs.readFileSync('./src/demo-spec.html', 'utf-8');
       const { document } = (new JSDOM(demo)).window;
       const peopleDisplay = document.querySelector('ul[data-id="people-display"]'),
             addPersonBtn = document.querySelector('button[data-id="addPerson"]');
-      
+
       
       function buildElement () {
         const listElem = document.createElement('li'),
@@ -24,7 +24,7 @@ describe('Demo App', ()=> {
               spanDelete = document.createElement('span'),
               spanEdit = document.createElement('span');
   
-        spanName.innerHTML = 'kevin';
+        spanName.innerHTML = 'jane';
         spanDelete.innerHTML = ' x';
         spanDelete.setAttribute('data-id','deletePerson');
         spanEdit.innerHTML = ' e';
@@ -40,7 +40,7 @@ describe('Demo App', ()=> {
       function addPerson () {
         peopleDisplay.appendChild(buildElement());
         sinon.spy(initialDB,'addToDB');
-        initialDB.addToDB('kevin');
+        initialDB.addToDB('jane');
       }
   
       addPersonBtn.addEventListener('click', addPerson);
@@ -49,35 +49,35 @@ describe('Demo App', ()=> {
       addPersonBtn.dispatchEvent(event);
       
       // Expect DOM List
-      expect(peopleDisplay.children.length).to.equal(2);
-      expect(peopleDisplay.children[1].tagName).to.equal('LI');
+      expect(peopleDisplay.children.length).to.equal(3);
+      expect(peopleDisplay.children[2].tagName).to.equal('LI');
       // Expect Database using sinon spy
       expect(initialDB).to.have.property('people');
       expect(initialDB.addToDB.callCount).to.equal(1);
-      expect(initialDB.people.length).to.equal(2);
+      expect(initialDB.people.length).to.equal(3);
       done();
     });
   });
   
   
-  describe('Delete A Person', ()=> {
-    it('Should delete a list element (person) and remove person from DB', (done)=> {
-      const demo = fs.readFileSync('./src/demo.html', 'utf-8');
+  describe('Delete A Person', () => {
+    it('Should delete a list element (person) and remove person from DB', (done) => {
+      const demo = fs.readFileSync('./src/demo-spec.html', 'utf-8');
       const { document } = (new JSDOM(demo)).window;
       const peopleDisplay = document.querySelector('ul[data-id="people-display"]'),
             deletePersonBtn = document.querySelector('span[data-id="deletePerson"]');
 
       MockInitialDB.expects('deleteFromDB').once()
         .withExactArgs('Darrell')
-        .returns(initialDB.people = [{id:2, name: 'kevin'}]); 
+        .returns(initialDB.people = [{id: 2, name: 'kevin'}]);
 
       function deletePerson (e) {
         const name = e.target.previousElementSibling.innerHTML;
-        initialDB.deleteFromDB(name);             
+        initialDB.deleteFromDB(name);
         let li = e.target.parentNode;
         li.remove();
       }
-      
+
       deletePersonBtn.addEventListener('click', deletePerson);
       var event = document.createEvent('HTMLEvents');
       event.initEvent('click', true, false);
@@ -87,7 +87,7 @@ describe('Demo App', ()=> {
       MockInitialDB.verify();
       expect(initialDB.people.length).to.equal(1);
       // Expect DOM List
-      expect(peopleDisplay.children.length).to.equal(0);
+      expect(peopleDisplay.children.length).to.equal(1);
       done();
     });
   });
